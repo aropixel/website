0e837789480d4a044d89575afd1751fef0b8e885
Joël Gomez Caballe <joel.gomez@aropixel.com>
2018-04-07 01:17:11 +0200

    Activités et Références

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	resizeLogo();
+	$(window).resize(resizeLogo);
+	$(window).scroll(hideArrow);
+
     setTimeout(function() {
         $('.aropixel').removeClass('aropixel--loading').addClass('aropixel--animate');
     }, 500);
@@ -12,4 +16,58 @@ $(document).ready(function() {
         $('.aropixel').addClass('aropixel--point');
     }, 3900);
 
+    setTimeout(function() {
+        $('.notvisible').addClass('show');
+    }, 4200);
+
+	/* ---------------------------------------------
+	 isotope | init Isotope
+	 --------------------------------------------- */
+
+	var $container = $(".portfolio:not(.portfolio-masonry)");
+	if ($.fn.imagesLoaded && $container.length > 0) {
+	    imagesLoaded($container, function () {
+	        setTimeout(function () {
+	            $container.isotope({
+	                itemSelector: '.portfolio-item',
+	                layoutMode: 'fitRows',
+	                filter: '*'
+	            });
+
+	            $(window).trigger("resize");
+	            // filter items on button click
+	        }, 500);
+
+	    });
+	}
+
 });
+
+function resizeLogo() {
+
+	var logoHeight = $(window).height() - 200;
+	var minLogoHeight = 500;
+
+	logoHeight = Math.max(logoHeight, minLogoHeight);
+	$('.aropixel-logo').height(logoHeight);
+
+
+	var sectionHeight = $(window).height();
+	var minSectionHeight = 700;
+
+	sectionHeight = Math.max(sectionHeight, minSectionHeight);
+	$('.aropixel-hero').height(sectionHeight);
+
+}
+
+function hideArrow() {
+
+	if ($(window).scrollTop() > 80) {
+		$('.arrow').fadeOut('slow');
+	}
+	else {
+		$('.arrow').fadeIn('fast');
+	}
+
+
+}
